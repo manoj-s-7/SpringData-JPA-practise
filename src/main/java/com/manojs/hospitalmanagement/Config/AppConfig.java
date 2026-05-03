b32679fa728b6dd330df4d81ad7c5323107552bf
Manoj <manojcs6317@gmail.com>
2026-05-03 18:33:13 +0530

    feat(Security): add AppConfig for password encoding and configure user roles in WebSecurityConfig
    Signed-off-by: Manoj <manojcs6317@gmail.com>

---
+++ src/main/java/com/manojs/hospitalmanagement/Config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.manojs.hospitalmanagement.Config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/manojs/hospitalmanagement/Config/WebSecurityConfig.java
+++ src/main/java/com/manojs/hospitalmanagement/Config/WebSecurityConfig.java
@@ -1,24 +1,52 @@
 package com.manojs.hospitalmanagement.Config;
 
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
+    private final PasswordEncoder passwordEncoder;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
         httpSecurity
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/**"
-                        ).authenticated()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/patients/**").hasAnyRole("ADMIN","PATIENT")
+                        .requestMatchers("/doctors/**").hasAnyRole("ADMIN","DOCTOR")
                         .anyRequest().permitAll()
                 )
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailService(){
+        UserDetails user1 = User.withUsername("admin")
+                .password(passwordEncoder.encode("pass"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.withUsername("patient")
+                .password(passwordEncoder.encode("pass"))
+                .roles("PATIENT")
+                .build();
+
+        UserDetails user3 = User.withUsername("doctor")
+                .password(passwordEncoder.encode("pass"))
+                .roles("DOCTOR")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,user2,user3);
     }
 }
